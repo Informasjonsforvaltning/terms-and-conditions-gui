41a2acacb387621a03725e5ed49a77f8d31e0d3e
Alexander Zakharov <alzak@envisage.no>
2021-05-26 13:27:40 +0200

    chore: remove unused babel plugin

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
-    'react-hot-loader/babel',
     'babel-plugin-styled-components'
   ]
 };
