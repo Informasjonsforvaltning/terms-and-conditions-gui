7d4d5dc05c786f1fd9206a3c12d3fbf5d042ffa0
Alexander Zakharov <alzak@envisage.no>
2021-05-26 12:50:13 +0200

    chore: use Footer component from component library

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,43 +1,29 @@
 import React, { memo, FC } from 'react';
-import EmailIcon from '@material-ui/icons/Email';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import { compose } from 'redux';
+import FooterBase from '@fellesdatakatalog/internal-footer';
+import { ThemeProfile } from '@fellesdatakatalog/theme';
 
-import SC from './styled';
+import env from '../../env';
 
-const Footer: FC = () => (
-  <SC.Footer>
-    <SC.Illustration />
-    <SC.Content>
-      <SC.Column>
-        Digitaliseringsdirektoratet forvalter Felles datakatalog.
-      </SC.Column>
-      <SC.Column>
-        <SC.Link href='https://www.digdir.no/om-oss/personvernerklaering/706'>
-          Personvernerklæring
-        </SC.Link>
-        <SC.Link href='https://www.digdir.no/om-oss/informasjonskapsler/707'>
-          Informasjonskapsler
-        </SC.Link>
-      </SC.Column>
-      <SC.Column>
-        <SC.LinkWrapper>
-          <EmailIcon />
-          <SC.Link href='mailto:fellesdatakatalog@digdir.no'>
-            fellesdatakatalog@digdir.no
-          </SC.Link>
-        </SC.LinkWrapper>
-        <SC.LinkWrapper>
-          <TwitterIcon />
-          <SC.Link
-            href='https://twitter.com/datakatalogen?ref_src=twsrc%5Etfw%7Ctwcamp%5Eembeddedtimeline%7Ctwterm%5Eprofile%3Adatakatalogen&ref_url=https%3A%2F%2Ffellesdatakatalog.digdir.no%2F'
-            external
-          >
-            Felles datakatalog på Twitter
-          </SC.Link>
-        </SC.LinkWrapper>
-      </SC.Column>
-    </SC.Content>
-  </SC.Footer>
-);
+import { withAuth, Props as AuthProps } from '../../providers/auth';
 
-export default memo(Footer);
+const { SKE_THEME_PROFILE } = env;
+
+interface Props extends AuthProps {}
+
+const Footer: FC<Props> = ({ authService }) => {
+  const isSkatteetatenThemeProfile =
+    authService
+      .getResourceRoles()
+      .some(({ resourceId }) =>
+        SKE_THEME_PROFILE?.split(',').includes(resourceId)
+      ) || !!localStorage.getItem('skeProfile');
+
+  const themeProfile = isSkatteetatenThemeProfile
+    ? ThemeProfile.SKE
+    : ThemeProfile.FDK;
+
+  return <FooterBase themeProfile={themeProfile} />;
+};
+
+export default compose<FC>(memo, withAuth)(Footer);
