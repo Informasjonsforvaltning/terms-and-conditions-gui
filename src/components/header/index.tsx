fc8dde676acb27aa36685d8b8b56c4537b2597c5
Alexander Zakharov <alzak@envisage.no>
2021-05-26 12:51:21 +0200

    chore: fix Header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,8 @@
 import React, { memo, FC } from 'react';
-import InternalHeader from '@fellesdatakatalog/internal-header';
+import { compose } from 'redux';
+import HeaderBase from '@fellesdatakatalog/internal-header';
 import Link from '@fellesdatakatalog/link';
+import { ThemeProfile } from '@fellesdatakatalog/theme';
 
 import env from '../../env';
 
@@ -8,25 +10,42 @@ import { withAuth, Props as AuthProps } from '../../providers/auth';
 
 interface Props extends AuthProps {}
 
-const { ADMIN_GUI_HOST, FDK_REGISTRATION_BASE_URI, SEARCH_HOST } = env;
+const {
+  ADMIN_GUI_HOST,
+  FDK_REGISTRATION_BASE_URI,
+  SEARCH_HOST,
+  SKE_THEME_PROFILE
+} = env;
 
 const Header: FC<Props> = ({ authService }) => {
-  const userName = authService.getUser()?.name;
-  const logOutAndRedirect = () => authService.logout();
+  const signOut = () => authService.signOut();
+
+  const isSkatteetatenThemeProfile =
+    authService
+      .getResourceRoles()
+      .some(({ resourceId }) =>
+        SKE_THEME_PROFILE?.split(',').includes(resourceId)
+      ) || !!localStorage.getItem('skeProfile');
+
+  const themeProfile = isSkatteetatenThemeProfile
+    ? ThemeProfile.SKE
+    : ThemeProfile.FDK;
 
   return (
-    <InternalHeader
+    <HeaderBase
       homeUrl={FDK_REGISTRATION_BASE_URI}
-      username={userName}
-      onLogout={logOutAndRedirect}
+      themeProfile={themeProfile}
+      username={authService.getUserProfile()?.name}
+      onLogout={signOut}
+      skeHomeText='Bruksvilkår'
     >
       <Link href={`${SEARCH_HOST}/guidance`}>Registrere data</Link>
       <Link href={ADMIN_GUI_HOST}>Høste data</Link>
       <Link href={SEARCH_HOST} external>
         Søk i Felles datakatalog
       </Link>
-    </InternalHeader>
+    </HeaderBase>
   );
 };
 
-export default memo(withAuth(Header));
+export default compose<FC>(memo, withAuth)(Header);
